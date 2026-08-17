fb0fb92c9c24941e2875d164bbd0a5069b28d6bd
Nokuphiwa <shangenokuphiwa683@gmail.com>
2026-08-17 15:52:49 +0200

    navStyle

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -4,11 +4,13 @@ import home from "../../assets/home icon.png";
 
 export const Navbar = () => {
   return (
+    <div className={styles.container}>
     <div className={styles.navbarContent}>
       <nav>
           <img src={home} alt="home"className={styles.image} />
           <a href= "/" className={styles.tittle}>login</a>
       </nav>
+    </div>
     </div>
   );
 };
