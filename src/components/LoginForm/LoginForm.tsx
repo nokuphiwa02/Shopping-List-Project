c4b5599634c4a38ec9012012ca684fc386039ece
Nokuphiwa <shangenokuphiwa683@gmail.com>
2026-08-18 14:22:19 +0200

    login page

--- src/components/LoginForm/LoginForm.tsx
+++ src/components/LoginForm/LoginForm.tsx
@@ -3,24 +3,24 @@ import styles from "./LoginForm.module.css";
 export const LoginForm = () => {
   return (
     <div className={styles.loginContainer}>
-      <div className="login-card">
-        <h1>LOGIN</h1>
+      <div className={styles.loginCard}>
+        <h1 className={styles.tittle}>LOGIN</h1>
 
-        <div className="form-group">
+        <div className={styles.email}>
           <label>Email Address:</label>
-          <input type="email" />
+          <input type="email" className={styles.emailInput} />
         </div>
 
-        <div className="form-group">
+        <div className={styles.password}>
           <label>Password:</label>
-          <input type="password" />
+          <input type="password" className={styles.passwordInput} />
         </div>
 
-        <button className="login-button">Sign In</button>
+        <button className={styles.loginBtn}>Sign In</button>
 
-        <button className="register-button">Sign up</button>
+        <button className={styles.registerBtn}>Sign up</button>
 
-        <p className="account-text">create an Account?</p>
+        <p className={styles.account}>create an Account?</p>
       </div>
     </div>
   );
