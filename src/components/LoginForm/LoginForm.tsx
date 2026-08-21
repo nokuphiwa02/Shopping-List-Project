ab9360e025364f08d06c5af05a4f9ed08c8fb213
Nokuphiwa <shangenokuphiwa683@gmail.com>
2026-08-21 09:04:27 +0200

    fixing login slice

--- src/components/LoginForm/LoginForm.tsx
+++ src/components/LoginForm/LoginForm.tsx
@@ -1,5 +1,13 @@
 import styles from "./LoginForm.module.css";
 import { useNavigate } from "react-router";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  updateEmailAddress,
+  updatePassword,
+  LoginThunk,
+} from "../../redux/features/LoginSlices";
+import type { RootState } from "../../../store";
+import type { AppDispatch } from "../../../store";
 
 export const LoginForm = () => {
   const navigate = useNavigate();
@@ -9,22 +17,56 @@ export const LoginForm = () => {
 
   const Login = useNavigate();
   const signIn = () => {
-  Login("/homepage");
+    Login("/homepage");
+  };
+
+  const dispatch = useDispatch<AppDispatch>();
+
+  const { email, password } = useSelector((state: RootState) => state.signIn);
+  const handleSave = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const result = await dispatch(LoginThunk({ email, password }));
+    if (LoginThunk.fulfilled.match(result)) {
+      alert("Login successful");
+      navigate("/homepage");
+    }
+   if (LoginThunk.rejected.match(result)) {
+  
+  const payload = result.payload as { status?: number; message?: string } | undefined;
+  
+  const errorMessage = payload?.message || result.error?.message;
+
+  if (errorMessage === "user not found" || payload?.status === 404) {
+    alert("User not found. Please check your email or sign up.");
+    navigate("/registerPage");
+  } else {
+    alert("Login failed. Please try again.");
+  }
+}
+
   };
 
   return (
-    <div className={styles.loginContainer}>
+    <form onSubmit={handleSave} className={styles.loginContainer}>
       <div className={styles.loginCard}>
         <h1 className={styles.tittle}>LOGIN</h1>
 
         <div className={styles.email}>
           <label>Email Address:</label>
-          <input type="email" className={styles.emailInput} />
+          <input
+            type="email"
+            className={styles.emailInput}
+            onChange={(e) => dispatch(updateEmailAddress(e.target.value))}
+          />
         </div>
 
         <div className={styles.password}>
           <label>Password:</label>
-          <input type="password" className={styles.passwordInput} />
+          <input
+            type="password"
+            className={styles.passwordInput}
+            onChange={(e) => dispatch(updatePassword(e.target.value))}
+          />
         </div>
 
         <button className={styles.loginBtn} onClick={signIn}>
@@ -37,6 +79,6 @@ export const LoginForm = () => {
 
         <p className={styles.account}>create an Account?</p>
       </div>
-    </div>
+    </form>
   );
 };
--- src/redux/features/LoginSlices.ts
+++ src/redux/features/LoginSlices.ts
@@ -1,5 +1,6 @@
 import { createSlice } from '@reduxjs/toolkit'
 import {type PayloadAction } from '@reduxjs/toolkit'
+import { createAsyncThunk } from '@reduxjs/toolkit'
 
 
 export interface User{
@@ -20,7 +21,25 @@ const initialState: LoginState = {
   error: null
 }
 
-export const LoginSlice = createSlice({
+export const LoginThunk = createAsyncThunk("users/LoginThunk",
+    async(newUser: Omit<User, "id"> ) => {
+        const response = await fetch("http://localhost:3000/users",
+            {
+                method: "GET",
+                headers: {
+                    "Contene-Type": "application/json"
+                },
+                body: JSON.stringify(newUser),
+            });
+            if(!response.ok){
+                throw new Error("failed to get server")
+            }
+            const data = await response.json();
+            return data;
+    }
+)
+
+    export const LoginSlice = createSlice({
     name: 'login',
     initialState,
     reducers: {
@@ -30,7 +49,21 @@ export const LoginSlice = createSlice({
       updatePassword: (state,action:PayloadAction<string>) => {
      state.password= action.payload
      },
-    }
+    },
+          extraReducers: (builder) => {
+            builder.addCase(LoginThunk.pending, (state) =>{
+                state.isLoading = true;
+                state.error =null
+            });
+            builder.addCase(LoginThunk.fulfilled, (state) => {
+                state.isLoading = false;
+    
+            });
+            builder.addCase(LoginThunk.rejected, (state ,action)=> {
+                state.isLoading=false
+                state.error= action.payload as string
+            })
+        }
 })
 
 
